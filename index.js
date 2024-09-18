14358d09d46c02a6b53ca6361fafa311877c4766
Rishabh Mishra <info@sopkom.com>
2024-09-18 19:33:44 +0530

    port changed

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const sequelize = require('./config/sequelize');
 const userRoutes = require('./routes/userRoutes');
 const app = express();
-const port = 3000;
+const port = 8080;
 
 app.use(express.json());
 // Serve uploaded files
