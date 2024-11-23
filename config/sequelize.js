a40c41c2f09d7290eebc86965870e2ae7ea47f63
Rishabh Mishra <info@sopkom.com>
2024-11-24 00:36:24 +0530

    removed node module and env

--- config/sequelize.js
+++ config/sequelize.js
@@ -1,9 +1,9 @@
 require('dotenv').config();
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize(process.env.DATABASE_NAME, process.env.DATABASE_USER, process.env.DATABASE_PASSWORD, {
-  host: process.env.DATABASE_HOST,
-  port: process.env.DATABASE_PORT,
+const sequelize = new Sequelize('ninjafleet', 'Rm765182', 'Rm765182', {
+  host: 'ninjacart.mysql.database.azure.com',
+ 
   dialect: 'mysql',
   dialectOptions: {
     ssl: {
